f9cf976ca2bebf9898bfe67228c9ba70816db026
tr14ngl3 <lenskii.ka@phystech.edu>
2026-02-23 00:57:17 +0300

    Fixed some more bugs

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,10 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'psosts':posts,})
+    return render(request, 'blog/post_list.html', {'posts':posts,})
 
 def view_post(request, handle):
     post = get_object_or_404(Post, handle=handle)
     post.text = safestring.mark_safe(post.text)
+    print(post)
     return render(request, 'blog/view_post.html', {'post':post})
